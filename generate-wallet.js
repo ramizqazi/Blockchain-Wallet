bed1451a5615f3acfa3ab5872a3b3201ac40b3b5
ramizqazi <ramizqazi03@gmail.com>
2023-08-29 04:33:02 -0700

    add generate wallet functionality

---
+++ generate-wallet.js
@@ -0,0 +1,19 @@
+import EC from 'elliptic';
+import { readFileSync, writeFileSync } from 'fs';
+
+const ec = new EC.ec('p192');
+
+const keyPair = ec.genKeyPair();
+
+const publicKey = keyPair.getPublic('hex');
+
+const newWalletName = process.argv[2];
+const walletsFile = readFileSync('./wallets.json');
+let wallets = JSON.parse(walletsFile);
+
+if(!wallets.hasOwnProperty(newWalletName)) {
+  wallets[newWalletName] = publicKey;
+  wallets = JSON.stringify(wallets, null, 2);
+
+  writeFileSync('./wallets.json', wallets);
+}
