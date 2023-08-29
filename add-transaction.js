2da51df5dd94d08a8da6769ffdb494a3eb02d5d3
ramizqazi <ramizqazi03@gmail.com>
2023-08-29 04:28:50 -0700

    add mine block functionality

---
+++ add-transaction.js
@@ -0,0 +1,44 @@
+import sha256 from 'crypto-js/sha256.js';
+import EC from 'elliptic';
+import {
+  writeTransactions,
+  getTransactions,
+  getAddressBalance,
+  getWalletAddressFromName
+} from './blockchain-helpers.js';
+
+const ec = new EC.ec('p192');
+
+const fromPrivateKey = process.argv[2];
+
+const toAddressName = process.argv[3];
+
+const amount = parseInt(process.argv[4]);
+
+const toAddress = getWalletAddressFromName(toAddressName);
+
+const fromKeyPair = ec.keyFromPrivate(fromPrivateKey, 'hex');
+
+const fromAddress = fromKeyPair.getPublic('hex');
+
+const hash = sha256(fromAddress + toAddress + amount).toString();
+
+const signature = fromKeyPair.sign(hash).toDER('hex');
+
+const newTransaction = {
+  hash,
+  amount,
+  toAddress,
+  signature,
+  fromAddress,
+}
+
+const transactions = getTransactions();
+const addressBalance = getAddressBalance(fromAddress);
+
+if (addressBalance >= amount) {
+  transactions.push(newTransaction);
+  writeTransactions(transactions);
+} else {
+  console.log('You do not have enough funds to make that transaction')
+}
